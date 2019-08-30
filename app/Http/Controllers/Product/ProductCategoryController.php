93058f2915e27742000eadf571ffa226c9c8960c
vagrant <vagrant@apirestfull>
2019-08-30 16:05:55 +0000

    Git Ignore para imags y Operaciones complejas

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Product;
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories()
+            ->get()
+            ->values()
+            ;
+
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product, Category $category)
+    {
+        /*
+            sync - reemplaza
+            attach - adiciona
+            syncWithoutDetaching - adiciona si no existe
+        */
+
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product, Category $category)
+    {
+        if(!$product->categories()->find($category->id)){
+            return $this->errorResponse('La categoria especificada no es una categoria de este producto', 404);
+        }
+
+        $product->categories()->detach([$category->id]);
+
+        return $this->showOne($product->categories);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -72,7 +72,7 @@ class User extends Authenticatable
     ];
 
     public function esVerificado(){
-        return $this->verified == User::USUARIO_ADMINISTRADOR;
+        return $this->verified == User::USUARIO_VERIFICADO;
     }
 
     public function esAdministrador(){
